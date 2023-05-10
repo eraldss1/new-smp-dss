7b3d736e1310b3f2c33394848e5b122cbf7f33c5
tonight-star <fitriauliaf123@gmail.com>
2023-05-10 12:45:55 +0700

    update criteria

--- lib/enums/enum_criteria.dart
+++ lib/enums/enum_criteria.dart
@@ -2,7 +2,7 @@ import 'package:new_smp_dss/models/criteria.dart';
 
 final List<Criteria> criterias = [
   Criteria(name: "Akreditasi", weight: 41, type: "Benefit"),
-  Criteria(name: "Jarak", weight: 31, type: "Benefit"),
-  Criteria(name: "SPP", weight: 21, type: "Benefit"),
+  Criteria(name: "Jarak", weight: 31, type: "Cost"),
+  Criteria(name: "SPP", weight: 21, type: "Cost"),
   Criteria(name: "Jumlah Murid Diterima", weight: 7, type: "Benefit"),
 ];
